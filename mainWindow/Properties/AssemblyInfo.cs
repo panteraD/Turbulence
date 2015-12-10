26f3f31e76406de19f2850c5d7237ac2fc8a9dda
doller <chernenkov.andrei@Yandex.ru>
2015-12-10 08:06:05 +0300

    added app-manifest

--- mainWindow/Properties/AssemblyInfo.cs
+++ mainWindow/Properties/AssemblyInfo.cs
@@ -7,10 +7,10 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("mainWindow")]
+[assembly: AssemblyTitle("Влияние Турбулентности на воздушное судно")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("МГТУ ГА")]
 [assembly: AssemblyProduct("mainWindow")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
